c92a4a188f9471d79ac0057d1de219f63a172b3f
Nataliya Kozulina <natasha.pechenka@gmail.com>
2025-04-03 09:44:01 +0500

    Set1 Configs
    Add Sprites and Configs

---
+++ Assets/Scripts/Game/Tiles/TileConfig.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Game.Tiles
+{
+    public enum TileKind
+    {
+        Normal,
+        Blabk,
+        Jelly
+    }
+    [CreateAssetMenu(fileName = "TileConfig", menuName = "Config/TileConfig")]
+    public class TileConfig : ScriptableObject
+    {
+        [SerializeField] private Sprite _sprite;
+        [SerializeField] private TileKind _tileKind;
+        [SerializeField] private bool _isInteractable;
+
+        public Sprite Sprite => _sprite;
+        public TileKind TileKind => _tileKind;
+        public bool IsInteractable => _isInteractable;
+    }
+}
