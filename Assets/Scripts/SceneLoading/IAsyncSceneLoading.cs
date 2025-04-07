506a40afcfde8d0a85be381bfd8b504e67f1479b
Nataliya Kozulina <natasha.pechenka@gmail.com>
2025-04-07 11:46:19 +0500

    Addresables and loadscenes

---
+++ Assets/Scripts/SceneLoading/IAsyncSceneLoading.cs
@@ -0,0 +1,11 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace SceneLoading
+{
+    public interface IAsyncSceneLoading
+    {
+        UniTask LoadAsync(string sceneName);
+        UniTask UnLoadAsync(string sceneName);
+        void LoadingDone(bool value);
+    }
+}
---
+++ Assets/Scripts/SceneLoading/LoadingView.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SceneLoading
+{
+    public class LoadingView : MonoBehaviour
+    {
+        [SerializeField] private GameObject _loadingScreen;
+
+        public void SetActiveScreen(bool value)
+        {
+            _loadingScreen.SetActive(value);
+        }
+    }
+}
