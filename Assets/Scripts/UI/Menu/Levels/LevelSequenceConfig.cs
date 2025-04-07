96b3f5cc1afba0d00122322d35e3563f7badcc1d
Nataliya Kozulina <natasha.pechenka@gmail.com>
2025-04-07 15:53:04 +0500

    Menu Entry point level sequence

---
+++ Assets/Scripts/UI/Menu/Levels/LevelSequenceConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Levels;
+using UnityEngine;
+
+namespace UI.Menu.Levels
+{
+    [CreateAssetMenu(fileName = "LevelSequenceConfigs", menuName = "Configs/LevelSequenceConfigs")]
+    public class LevelSequenceConfig : ScriptableObject
+    {
+        [SerializeField] private List<LevelConfig> _levelSequence = new List<LevelConfig>();
+
+        public List<LevelConfig> LevelSequence => _levelSequence;
+
+        private void OnValidate()
+        {
+            if (_levelSequence.Count != 5)
+            {
+                throw new ArgumentOutOfRangeException("Levels sequence must contain 5 elements");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Menu/MenuEntryPoint.cs
@@ -0,0 +1,29 @@
+﻿using SceneLoading;
+using UI.Menu.Levels;
+using VContainer.Unity;
+
+namespace UI.Menu
+{
+    public class MenuEntryPoint : IInitializable
+    {
+        private IAsyncSceneLoading _sceneLoading;
+        private SetupLevelSequence _setupLevelSequence;
+
+        public MenuEntryPoint(IAsyncSceneLoading sceneLoading, SetupLevelSequence setupLevelSequence)
+        {
+            _sceneLoading = sceneLoading;
+            _setupLevelSequence = setupLevelSequence;
+        }
+
+        public async void Initialize()
+        {
+            // setup level sequence
+            await _setupLevelSequence.Setup(1);
+            // music menu
+            // loading is done
+            _sceneLoading.LoadingDone(true);
+            // await animation
+            // buttons enable
+        }
+    }
+}
